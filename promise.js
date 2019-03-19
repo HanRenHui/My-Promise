30031776568b90f8f01d8dbea9137584a6b15a79
HanRenHui <897691781@qq.com>
2019-03-19 21:57:34 +0800

    fitst commmt

---
+++ promise.js
@@ -0,0 +1,139 @@
+// import { Module } from "module";
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+function Promise(fn) {
+  let self = this 
+  self.status = PENDING
+  self.FulfiledCallbacks = []
+  self.RejectCallbacks = []
+  function resolve(value) {
+    setTimeout(function() {
+      if(self.status === PENDING) {
+        self.status = FULFILLED
+        self.value = value
+        self.FulfiledCallbacks.forEach(r => r(value))
+      }
+    })
+  }
+  function reject(err) {
+    setTimeout(function() {
+      if(self.status === PENDING) {
+        self.status = REJECTED
+        self.err = err 
+        self.RejectCallbacks.forEach(r => r(err))
+      }
+    })
+  }
+  try {
+    fn(resolve, reject)
+  } catch (error) {
+    reject(error)
+  }
+}
+Promise.prototype.then = function(onFulfiled, onRejected) {
+  // 如果成功或失败的回调没有传， 则表示这个then没有任何逻辑，只会把值往后传
+  onFulfiled = typeof onFulfiled == 'function' ? onFulfiled : v => v
+  onRejected = typeof onRejected == 'function' ? onRejected : err => { throw err }
+  let promise2 
+  let self = this
+  if(this.status === PENDING) {
+    promise2 = new Promise((resolve, reject) => {
+      this.FulfiledCallbacks.push((value) => { 
+        try{
+          let x = onFulfiled(value)
+          resolvePromise(promise2, x, resolve, reject)
+        }catch(e) {
+
+          reject(e)
+        }
+          
+        })
+        this.RejectCallbacks.push(err => {
+          try{
+            let x = onRejected(err)
+            resolvePromise(promise2, x, resolve, reject)
+          }catch(e) {
+            reject(e)
+          } 
+      })
+    })
+  }
+          // 若是没有异步任务的话就直接执行
+  if(this.status === FULFILLED) {
+    promise2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try{
+          let x = onFulfiled(this.value)
+          // resolve(x)
+          resolvePromise(promise2, x, resolve, reject)
+        }catch(e){
+          reject(e)
+        }
+      })
+    })
+  }
+  if(this.status === REJECTED) {
+      promise2 = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        try{
+          let x = onRejected(this.err)
+          // resolve(x)
+          resolvePromise(promise2, x, resolve, reject)
+        }catch(e) {
+
+          reject(e)
+        } 
+      })
+    })
+  }
+  return promise2
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+  
+  if(x === promise2) {
+    return reject(new TypeError('promise2将无法执行'))
+  }
+  // flag如果resolve已经执行，则不要执行reject 
+  var called = false
+  var then
+  if( (x != null) && ((typeof x === 'function') || (typeof x === 'object'))) {
+    try{
+      then = x.then 
+      if(typeof then === 'function') {
+        then.call(x, function rs(y) {
+          if(called) return 
+          called = true
+          return resolvePromise(promise2, y, resolve, reject)
+        }, function rj(err) {
+          if(called) return 
+          called = true
+           reject(err)
+        })
+      }else {
+        // 不是thenabale对象的话， 直接传
+        resolve(x)
+      }
+    }catch(e) {
+      if(called) return 
+      called = true
+       reject(e)
+    }
+  }else {
+    resolve(x)
+  }
+}
+
+Promise.prototype.catch = function(onRejected) {
+  return this.then(null, onRejected)
+}
+Promise.deferred = Promise.defer = function() {
+  var defer ={}
+  defer.promise = new Promise((resolve, reject) => {
+    defer.resolve = resolve 
+    defer.reject = reject
+  })
+  return defer
+}
+ module.exports = Promise
